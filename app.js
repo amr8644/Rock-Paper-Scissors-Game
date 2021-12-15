db2587a22fc6d764092f02731d53951b06da2829
Amr Ashebo <amr8644@gmail.com>
2021-12-15 21:43:12 +0300

    Fixed Javascript

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const rules = document.querySelector(".rules-btn");
 const modal = document.querySelector(".modal");
 const closeModal = document.querySelector(".close-btn");
 const mainSection = document.querySelector(".main");
-// const playAgain = document.getElementsByClassName("play-again-btn");
+const inGame = document.querySelector(".in-game");
 
 function compChoice() {
   const options = ["rock", "paper", "scissors"];
@@ -20,7 +20,6 @@ function compChoice() {
   const comp = options[random];
   return comp;
 }
-
 function game(userChoice) {
   const computerChoice = compChoice();
   switch (userChoice + computerChoice) {
@@ -30,32 +29,36 @@ function game(userChoice) {
       let addPlayer = playerCount++;
       playerScore.innerHTML = `${addPlayer}`;
       text.textContent = `${userChoice} beats ${computerChoice}.`;
-      mainSection.innerHTML = `<div class="player-pick">
+      mainSection.style.display = "none";
+      inGame.style.display = "flex";
+      inGame.innerHTML = ` <div class="player-pick">
       <h1>You Picked</h1>
       <button class="${userChoice}"></button>
     </div>
     <div class="play-again">
-    <h1> YOU WIN </h1>
-      <button class="play-again-btn" onclick="playAgain()">Play Again</button>
+      <h1>YOU WIN</h1>
+      <button class="play-btn">Play Again</button>
     </div>
     <div class="house-pick">
       <h1>House Picked</h1>
       <button class="${computerChoice}"></button>
     </div>`;
       break;
-    case "rockppaper":
+    case "rockpaper":
     case "paperscissors":
     case "scissorsrock":
       let addComp = compCount++;
       compScore.innerHTML = `${addComp}`;
       text.innerHTML = `${computerChoice} beats ${userChoice}.`;
-      mainSection.innerHTML = `<div class="player-pick">
+      mainSection.style.display = "none";
+      inGame.style.display = "flex";
+      inGame.innerHTML = ` <div class="player-pick">
       <h1>You Picked</h1>
       <button class="${userChoice}"></button>
     </div>
     <div class="play-again">
-    <h1> YOU LOSE </h1>
-      <button class="play-again-btn" onclick="playAgain()">Play Again</button>
+      <h1>YOU LOST</h1>
+      <button class="play-btn">Play Again</button>
     </div>
     <div class="house-pick">
       <h1>House Picked</h1>
@@ -65,13 +68,15 @@ function game(userChoice) {
     case "rockrrock":
     case "paperpaper":
     case "scissorsscissors":
-      mainSection.innerHTML = `<div class="player-pick">
+      mainSection.style.display = "none";
+      inGame.style.display = "flex";
+      inGame.innerHTML = ` <div class="player-pick">
       <h1>You Picked</h1>
       <button class="${userChoice}"></button>
     </div>
     <div class="play-again">
-    <h1> IT'S A TIE </h1>
-      <button class="play-again-btn" onclick="playAgain()">Play Again</button>
+      <h1>IT'S A DRAW</h1>
+      <button class="play-btn">Play Again</button>
     </div>
     <div class="house-pick">
       <h1>House Picked</h1>
@@ -79,6 +84,11 @@ function game(userChoice) {
     </div>`;
       break;
   }
+  const playbtn = document.querySelector(".play-btn");
+  playbtn.addEventListener("click", () => {
+    mainSection.style.display = "flex";
+    inGame.style.display = "none";
+  });
 }
 
 function main() {
@@ -92,15 +102,11 @@ function main() {
     game("scissors");
   });
 }
-
 rules.addEventListener("click", () => {
   modal.classList.add("show-modal");
 });
 closeModal.addEventListener("click", () => {
   modal.classList.remove("show-modal");
 });
-function playAgain() {
-  location.reload();
-}
 
 main();
